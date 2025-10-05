bec5ce05e647874935d7794eb9f8277b4f41178d
Gokul R <144220646+Gokul11271@users.noreply.github.com>
2025-10-05 11:06:00 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="bg-blue-200 h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Hello Tailwind!</h1>
+      <div className="bg-red-500 text-white p-6">Hello Tailwind!</div>
+      <h1 className="text-blue-700 font-bold text-3xl">Welcome</h1>
+      <div className="bg-red-500 text-white h-32 w-32">Test</div>
+    </div>
+  );
+}
