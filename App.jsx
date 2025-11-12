31f76793f60f5f412205ab6ec1e96eb0e8308bd3
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2025-11-12 22:29:34 +0530

    added front end in react

--- App.jsx
+++ App.jsx
@@ -1,10 +1,9 @@
 export default function App() {
   return (
-    <div className="bg-blue-200 h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Hello Tailwind!</h1>
-      <div className="bg-red-500 text-white p-6">Hello Tailwind!</div>
-      <h1 className="text-blue-700 font-bold text-3xl">Welcome</h1>
-      <div className="bg-red-500 text-white h-32 w-32">Test</div>
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        College Management System Frontend
+      </h1>
     </div>
   );
 }
